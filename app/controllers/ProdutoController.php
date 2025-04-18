df1effe7a9bc5f7bd44354e81646868f01b91140
leomorae-s <leonardomoraes098@gmail.com>
2025-04-18 20:28:22 -0300

    Criado controllers de produto e de venda

--- app/controllers/ProdutoController.php
+++ app/controllers/ProdutoController.php
@@ -2,7 +2,68 @@
 
 namespace controllers;
 
+use PDO;
+
 class ProdutoController
 {
 
+    private $db;
+
+    public function __construct(){
+        $this->db = ((new \Database())->getConnection());
+    }
+
+    public function index(){
+        $sql = "SELECT * FROM produtos";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        return $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        }
+
+    public function show($id){
+        $sql = "SELECT * FROM produtos WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        return $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function create(){
+
+        $sql = "INSERT INTO produtos (nome, descricao, preco, categoria, estoque) 
+                VALUES (:nome, :descricao, :preco, :categoria, :estoque)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(":nome", $_POST['nome']);
+        $stmt->bindParam(":descricao", $_POST['descricao']);
+        $stmt->bindParam(":preco", $_POST['preco']);
+        $stmt->bindParam(":categoria", $_POST['categoria']);
+        $stmt->bindParam(":estoque", $_POST['estoque']);
+        $stmt->execute();
+        return $stmt->rowCount();
+
+    }
+
+    public function update($id, $data){
+        $sql = "UPDATE produtos 
+                SET nome = :nome, descricao = :descricao, preco = :preco,categoria= :catgoria, estoque = :estoque 
+                WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(":nome", $data['nome']);
+        $stmt->bindParam(":descricao", $data['descricao']);
+        $stmt->bindParam(":preco", $data['preco']);
+        $stmt->bindParam(":categoria", $data['categoria']);
+        $stmt->bindParam(":estoque", $data['estoque']);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        return $stmt->rowCount();
+    }
+
+    public function delete($id){
+        $sql = "DELETE FROM produtos WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        return $stmt->rowCount();
+    }
 }
--- app/controllers/VendaController.php
+++ app/controllers/VendaController.php
@@ -1,8 +1,66 @@
 <?php
 
 namespace controllers;
-
+require_once '../config/database.php';
+use PDO;
 class VendaController
 {
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = (new \Database())->getConnection();
+    }
+
+    public function index()
+    {
+        $sql = "SELECT * FROM vendas";
+        $stmt = $this->db->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function show($id)
+    {
+        $sql = "SELECT * FROM vendas WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function store($data)
+    {
+        $sql = "INSERT INTO vendas (id_vendedor, data_venda,valor_total, descricao)
+                VALUES (:id_vendedor, :data_venda, :valor_total, :descricao)";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([
+            'id_vendedor' => $data['id_vendedor'],
+            'data_venda' => $data['data_venda'],
+            'valor_total' => $data['valor_total'],
+            'descricao' => $data['descricao']
+        ]);
+    }
+
+    public function update($id, $data)
+    {
+        $sql = "UPDATE vendas 
+                SET id_vendedor = :id_vendedor, data_venda = :data_venda, valor_total = :valor_total, descricao = :descricao
+                WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([
+            'id' => $id,
+            'id_vendedor' => $data['id_vendedor'],
+            'data_venda' => $data['data_venda'],
+            'total' => $data['valor_total'],
+            'descricao' => $data['descricao']
+        ]);
+
+
+    }
+
+    public function delete($id) {
+        $sql = "DELETE FROM vendas WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute(['id' => $id]);
+    }
 
 }
